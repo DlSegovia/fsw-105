5cc70f3959e12da911d8f9e9debc7113e6786e96
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-01-07 17:29:30 -0600

    building_js_functions_to_keep_code_dry

--- week2/daily_planet_editor/paper.js
+++ week2/daily_planet_editor/paper.js
@@ -1,8 +1,57 @@
+// var enemiesName = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
+
+// function whoWins(isThereKryptonite, enemiesName) {
+//     {
+//         if (!isThereKryptonite) {
+//             return "Superman beats " + enemiesName + ", of course";
+//         } else {
+//             return "Depends on how quick Superman can get rid of the Kryptonite. " + enemiesName + " could possibly win this one.";
+//         }
+
+//         function
+//         whoWins(isThereKryptonite, enemiesName) {
+//             if (!isThereKryptonite) {
+//                 return "Superman beats " + enemiesName + ", of course";
+//             } else {
+//                 return "Depends on how quick Superman can get rid of the Kryptonite. " + Enemies + " could possibly win this one.";
+//             }
+//         }
+//         for (var i = 0; i < enemiesName.length; i++) {
+//             var isThereKryptonite;
+
+//             if (i % 2 === 0) {
+//                 isThereKryptonite = true;
+//             } else {
+//                 isThereKryptonite = false;
+//             }
+//             console.log(whoWins(isThereKryptonite, enemiesName[i]));
+//         }
+
+//         function howAttractedIsLoisLaneToMe() {
+//             // 1 is not at all attracted, 10 is "super" attracted...
+//             return Math.floor((Math.random() * 10) + 1);
+//         }
+
+//         console.log(howAttractedIsLoisLaneToMe());
+
+//         var clarkKent = true;
+//         var superman = false;
+
+//         while (clarkKent) {
+//             console.log("I'm just a nerdy columnist");
+//             var phoneBoothQuickChange = Math.random();
+//             if (phoneBoothQuickChange >= 0.5) {
+//                 clarkKent = false;
+//                 superman = true;
+//                 console.log("Now I'm Superman!");
+//             }
+
+// Written by Kent, Clark
+
+
 var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-function
-whoWins(isThereKryptonite, enemies) {
-
+funtion whoWins(isThereKryptonite, enemies); {
     if (!isThereKryptonite) {
         return "Superman beats " + enemies + ", of course";
     } else {
@@ -12,27 +61,40 @@ whoWins(isThereKryptonite, enemies) {
 
 for (var i = 0; i < enemies.length; i++) {
     var isThereKryptonite;
-
     if (i % 2 === 0) {
         isThereKryptonite = true;
     } else {
         isThereKryptonite = false;
     }
-    console.log(whoWins("is there kryptonite" + enemies + [i]));
+    console.log(whoWins(isThereKryptonite, enemies[i]));
+    if (i % 2 === 0) {
+        isThereKryptonite = true;
+    } else {
+        isThereKryptonite = false;
+    }
+    console.lg(whoWins(isThereKryptonite + enemies + [i]));
 }
 
-function howAttractedIsLoisLaneToMe() {
-    // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Math.random() * 10) + 1);
-}
+function HowAttractedIsLoisLaneToMe() {
+    function HowAttractedIsLoisLaneToMe() {
+        // 1 is not at all attracted, 10 is "super" attracted...
+        return Math.floor((Mat.random() * 10) + 1);
+    }
 
-console.log(howAttractedIsLoisLaneToMe());
+    consol.log(HowAttractedIsLoisLaneToMe());
 
-var clarkKent = true;
-var superman = false;
+    var clarkKent = true;
+    var superman = false;
 
-while (clarkKent) {
-    console.log("I'm just a nerdy columnist");
+    while (clarkKent) {
+        console.log("I'm just a nerdy columnist");
+        var phoneBoothQuickChange = Math.random();
+        if (phoneBoothQuickChange >= 0.5) {
+            clarkKent = false;
+            superman = true;
+            console.log("Now I'm Superman!");
+        }
+    }
     var phoneBoothQuickChange = Math.random();
     if (phoneBoothQuickChange >= 0.5) {
         clarkKent = false;
@@ -40,3 +102,5 @@ while (clarkKent) {
         console.log("Now I'm Superman!");
     }
 }
+//     }
+// }
--- week3/loops_and_arrays/index.js
+++ week3/loops_and_arrays/index.js
@@ -1,3 +1,5 @@
+// Daniel Segovia class fsw-105 week3 Loops and Arrays
+// ----------------------------------------------- this is the computer inventory
 var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"];
 let counter = 0;
 for (let i = 0; i < officeItems.length; i++) {
@@ -6,7 +8,7 @@ for (let i = 0; i < officeItems.length; i++) {
     }
 }
 console.log("We found " + counter + " computers");
-
+// ----------------------------------------------- this is the movie goers names, age, and gender
 var peopleWhoWantToSeeMadMaxFuryRoad = [{
     name: "Mike",
     age: 12,
@@ -30,19 +32,50 @@ var peopleWhoWantToSeeMadMaxFuryRoad = [{
 }];
 
 for (var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
-    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18 && peopleWhoWantToSeeMadMaxFuryRoad[i].gender)
-        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough." + peopleWhoWantToSeeMadMaxFuryRoad[i].gender + " to see Mad Max Fury Road.");
-    else if (peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18 && peopleWhoWantToSeeMadMaxFuryRoad[i].gender)
-        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max Fury Road. Don't let the " + peopleWhoWantToSeeMadMaxFuryRoad[i].gender + " in.");
-
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) { //The first if statement checks for age/and gender for females
+        if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female")
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. She's good to see Mad Max Fury Road.");
+    } else if (peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18) {
+        if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female")
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max Fury Road. Do not let her in.");
+    }
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) { //The second if statement checks for age/and gender for males
+        if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "male")
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. He's good to see Mad Max Fury Road.");
+    } else if (peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18) {
+        if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "male")
+            console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max Fury Road. Do not let him in.");
+    }
 }
 var number = 0;
 for (var i = 0; i < 100; i++) {
     if (number % 2 === 0) {
-        console.log(number + " is even number <br>");
+        console.log(number + " is even number");
         number = number + 1;
     } else {
-        console.log(number + " is odd number <br>");
+        console.log(number + " is odd number");
         number = number + 1;
+    }
+}
+// ----------------------------------------------- this is the extra credit for on/off light switch
+var arrayOfArrays = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for (var i = 0; i < arrayOfArrays.length; i++) { // how many times will this loop run = 3
+    var countSwitch = 0
+    for (var j = 0; j < arrayOfArrays[i].length; j++) { // how many times will this loop run = 9
+        countSwitch = countSwitch + arrayOfArrays[i][j]
+
+    }
+
+    if (countSwitch % 2 === 0) {
+        console.log(arrayOfArrays[i] + " Light's are off");
+
+    } else {
+        console.log(arrayOfArrays[i] + " Light's are on");
+
     }
 }
