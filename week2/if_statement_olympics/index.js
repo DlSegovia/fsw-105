366089319f307348eb24b58aaa087c807587e7b5
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-01-07 19:02:29 -0600

    Updated file Daniel
    Was unable to update file from the desktop

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -1,48 +1,45 @@
-// 
+// If statement olympics
+if (5 > 3) {
+    console.log("is greater than")
+}
 
+if (catString.length === 3) {
+    console.log("is the length")
+}
 
-
-// if (5 > 3) {
-//     console.log("is greater than")
-// }
-
-// if (catString.length === 3) {
-//     console.log("is the length")
-// }
-
-// if (catString === dogString) {
-//     console.log("the same")
-// } else {
-//     console.log("not the same")
-// }
+if (catString === dogString) {
+    console.log("the same")
+} else {
+    console.log("not the same")
+}
 
 var person = {
-        //     name: "Bobby",
-        //     age: 12
+        name: "Bobby",
+        age: 12
 
-        for (let i = 0; i < person.length; i++)
-            if (person.age > 18) {
-                console.log("is old enough to go to the movie")
-            } else {
-                console.log("is not old enough to go to the movie")
-            }
+for (let i = 0; i < person.length; i++)
+if (person.age > 18) {
+        console.log("is old enough to go to the movie")
+} else {
+        console.log("is not old enough to go to the movie")
+}
 
-            // var person = {
-            //     name: "Bobby",
-            //     age: 12
-            // }
-            // if (person.name[0] === 'B') {
-            //     console.log(person.name + " is allowed to go to the movie")
-            // } else {
-            //     console.log(person.name + " is not allowed to go to the movie")
-            // }
+var person = {
+        name: "Bobby",
+        age: 12
+}
+if (person.name[0] === 'B') {
+        console.log(person.name + " is allowed to go to the movie")
+} else {
+        console.log(person.name + " is not allowed to go to the movie")
+}
 
-            // var person = {
-            //     name: "Bobby",
-            //     age: 12
-            // }
-            // if (person.name[0] === "B" && person.age > 18) {
-            //     console.log(person.name + " is allowed to go to the movie")
-            // } else {
-            //     console.log(person.name + " is not allowed to go to the movie")
-            // }
+var person = {
+        name: "Bobby",
+        age: 12
+}
+if (person.name[0] === "B" && person.age > 18) {
+        console.log(person.name + " is allowed to go to the movie")
+} else {
+        console.log(person.name + " is not allowed to go to the movie")
+}
