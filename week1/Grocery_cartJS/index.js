da9762456888607a907c06adffd224e3e1a267b5
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-01-21 14:06:58 -0600

    update file

--- week1/Grocery_cartJS/index.js
+++ week1/Grocery_cartJS/index.js
@@ -3,11 +3,11 @@ var shopper = {
     lastName: "Doe",
     shopperId: 101,
     active: true,
-    groceryCart: ["Apples", "Bananas", "Milk", "Cereal"],
+    groceryCart: ["apples", " bananas", " milk and cereal"],
     fullName: function() {
-        return "Hello, " + this.firstName + " " + this.lastName + " " + "Active" + " " + this.active + " " + "Grocery items" + " " + this.groceryCart["3"];
+        return "Hello, " + this.firstName + " " + this.lastName + " " + "are a member? " + this.active + "," + " Your grocery cart has" + " " + this.groceryCart + ".";
     }
 
 };
-console.log(shopper)
-    // document.getElementById("demo").innerHTML = shopper.fullName(); // Remeber to uncomment when running HTML
+console.log(shopper.fullName());
+// document.getElementById("demo").innerHTML = shopper.fullName(); // Remeber to uncomment when running HTML
