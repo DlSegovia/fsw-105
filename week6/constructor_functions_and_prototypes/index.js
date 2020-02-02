05b9057891775b0960057bc513dfa0beb10f8af3
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-02-02 13:44:33 -0600

    week 7 assignment

--- week6/constructor_functions_and_prototypes/index.js
+++ week6/constructor_functions_and_prototypes/index.js
@@ -1,25 +1,32 @@
 // Daniel Segovia fsw-105 week6 assignment
-const array = []
+const arrayEmp = [];
 
-function Employee(name, jobTitle, salary, status) {
-    this.name = name
-    this.jobTitle = jobTitle
-    this.salary = salary
-    this.status = status
+function Employee(name, jobTitle, salary, status = "Full Time") {
+    this.name = name;
+    this.jobTitle = jobTitle;
+    this.salary = salary;
+    this.status = status;
     this.printEmployeeForm = function() {
-        console.log("Employee " + this.name + " works as a " + this.jobTitle + " earning salary of " + this.salary + " a month and works as a " + this.status + " employee.")
+        console.log("Employee " + this.name + " works as a " + this.jobTitle + " earning salary of " + this.salary + " a month and works as a " + this.status + " employee.");
     }
     this.changeStatus = function(newStatus) {
         this.status = newStatus
     }
 }
-var employeeOne = new Employee("Dan Doe", "Sales Person", 3500 + "k", "Full Time")
-var employeeTwo = new Employee("Debbie Cool", "Cashier", 2500 + "k", "Full Time")
-var employeeThree = new Employee("Mark Parker", "Manager", 4000 + "k", "Full Time")
+var employeeOne = new Employee("Dan Doe", "Sales Person", 35 + "k", "Full Time");
+var employeeTwo = new Employee("Debbie Cool", "Cashier", 25 + "k", "Full Time");
+var employeeThree = new Employee("Mark Parker", "Manager", 40 + "k", "Full Time");
 
-array.push(employeeOne, employeeTwo, employeeThree);
-employeeOne.changeStatus("Contract")
+// array.push(employeeOne, employeeTwo, employeeThree);
+// employeeOne.changeStatus("Contract")
 
-employeeOne.printEmployeeForm()
-employeeTwo.printEmployeeForm()
-employeeThree.printEmployeeForm()
+function addNew(empl) {
+    arrayEmp.push(empl)
+}
+addNew(employeeOne);
+addNew(employeeTwo);
+addNew(employeeThree);
+// employeeOne.printEmployeeForm()
+// employeeTwo.printEmployeeForm()
+// employeeThree.printEmployeeForm()
+console.log(arrayEmp);
---
+++ week6/escape_room_simulation/index.js
@@ -0,0 +1,27 @@
+const readlineSync = newFunction()
+
+const userName = readlineSync.question('What is your name?: ')
+
+readlineSync.question(userName + ' You are about to embark on a new adventure. This journey is fraught with many dangers and very well be fatal to your health. When you are prepared, press anyy key and hit "enter",to brgin.")
+
+        const enemies =
+
+            switch (userAction) {
+                case runAway = Math.random()
+                if (runAway < .5) {
+                    console.log("You attempt to run away. But before you can, " + enemy + " attacks you for: ", enemyAttackPower)
+                } else {
+                    console.log("You ran away")
+                    break whileLoop
+                }
+                case "b":
+                    munc console.log("You just attacked " + enemy + " for" + userAttackPower + " attack power")
+
+                    enemyHP -= userActionPower
+                    console.log("Enemy just" + enemy + " for" + userAttackPower + " attack power")
+
+            }
+
+        function newFunction() {
+            return require('readline-sync')
+        }
